f8fecd9dc23555026a46adfc4403585e19c47a58
mykola9208 <117111441+mykola9208@users.noreply.github.com>
2022-11-13 21:23:04 +0300

    homework number 2

---
+++ homework2.py
@@ -0,0 +1,32 @@
+def maximum(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def minimum(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+
+
+def module(a):
+    if a > 0:
+        return a
+    else:
+        return -a
+
+
+def sum_numbers(a, b):
+    print(a+b)
+
+
+def is_positive(a):
+    if a > 0:
+        print('The number is positive')
+    elif a < 0:
+        print('The number is negative')
+    else:
+        print('The number is zero')
